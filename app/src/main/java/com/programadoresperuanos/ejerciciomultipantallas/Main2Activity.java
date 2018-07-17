e7709f24e6cfb00d7444ec26418390eb66dd23f9
joelcautirojas <joel.cauti@gmail.com>
2018-07-16 21:28:55 -0500

    primera version

---
+++ app/src/main/java/com/programadoresperuanos/ejerciciomultipantallas/Main2Activity.java
@@ -0,0 +1,26 @@
+package com.programadoresperuanos.ejerciciomultipantallas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    Button b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        b2 = findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(getApplicationContext(),Main3Activity.class);
+                startActivity(i1);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/programadoresperuanos/ejerciciomultipantallas/Main3Activity.java
@@ -0,0 +1,26 @@
+package com.programadoresperuanos.ejerciciomultipantallas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main3Activity extends AppCompatActivity {
+
+    Button b3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        b3 = findViewById(R.id.button3);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i1);
+            }
+        });
+    }
+}
